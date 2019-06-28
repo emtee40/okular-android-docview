1c3da086b689c8bb5407ddf4909c84f03e5f37f7
Katarina Behrens <Katarina.Behrens@cib.de>
2019-06-28 13:38:43 +0200

    Signature mode must eventually end

--- ui/pageviewannotator.cpp
+++ ui/pageviewannotator.cpp
@@ -1112,6 +1112,8 @@ void PageViewAnnotator::detachAnnotation()
         selectTool(-1);
         if (m_actionHandler)
             m_actionHandler->deselectAllAnnotationActions();
+    } else {
+        setSignatureMode(false);
     }
 }
 
