9ddb3c3e021e61e616d196ea5bfeecbf483ba426
Katarina Behrens <Katarina.Behrens@cib.de>
2019-08-16 12:11:02 +0200

    Stub some iface for signing the document

--- core/document.cpp
+++ core/document.cpp
@@ -2707,6 +2707,12 @@ bool Document::canConfigurePrinter() const
         return false;
 }
 
+void Document::sign()
+{
+    if (d->m_generator->canSign())
+        d->m_generator->sign();
+}
+
 DocumentInfo Document::documentInfo() const
 {
     QSet<DocumentInfo::Key> keys;
