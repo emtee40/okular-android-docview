23042a797881af35ff8104d003f6111c59ee26b5
David Hurka <david.hurka@mailbox.org>
2020-08-29 18:19:01 +0200

    Combine titlebar config controls to one combobox
    This replaces the “Display document title if available” checkbox
    and the “Display document file name / full path” group box
    by a single combobox with 4 items.
    
    + Only one control for one config thing
    - The user is assumed to be smart enough to figure out that “Document title or ...”
      means to use the title if it is available, and the file name/path otherwise.

--- conf/dlggeneral.cpp
+++ conf/dlggeneral.cpp
@@ -24,7 +24,7 @@ DlgGeneral::DlgGeneral(QWidget *parent, Okular::EmbedMode embedMode)
 
     if (embedMode == Okular::ViewerWidgetMode) {
         m_dlg->kcfg_SyncThumbnailsViewport->setVisible(false);
-        m_dlg->kcfg_DisplayDocumentTitle->setVisible(false);
+        m_dlg->titlebarControlCombobox->setVisible(false);
         m_dlg->kcfg_WatchFile->setVisible(false);
         m_dlg->kcfg_rtlReadingDirection->setVisible(false);
     }
@@ -52,4 +52,57 @@ void DlgGeneral::showEvent(QShowEvent *)
     else
         m_dlg->kcfg_ObeyDRM->hide();
 #endif
+}
+
+TitlebarControlCombobox::TitlebarControlCombobox(QWidget* parent)
+    : QComboBox(parent)
+    , m_displayDocumentTitle(new KcfgPropertyProxy(QStringLiteral("kcfg_DisplayDocumentTitle"), this))
+    , m_displayDocumentNameOrPath(new KcfgPropertyProxy(QStringLiteral("kcfg_DisplayDocumentNameOrPath"),this))
+{
+    addItem(i18nc("@item:inlistbox Config dialog, general page", "Document file name"));
+    addItem(i18nc("@item:inlistbox Config dialog, general page", "Document file path"));
+    addItem(i18nc("@item:inlistbox Config dialog, general page", "Document title or file name"));
+    addItem(i18nc("@item:inlistbox Config dialog, general page", "Document title or file path"));
+
+    connect(this, qOverload<int>(&QComboBox::currentIndexChanged), this, &TitlebarControlCombobox::slotIndexChanged);
+
+    connect(m_displayDocumentTitle, &KcfgPropertyProxy::user_proxyPropertyChanged, this, &TitlebarControlCombobox::slotConfigChanged);
+    connect(m_displayDocumentNameOrPath, &KcfgPropertyProxy::user_proxyPropertyChanged, this, &TitlebarControlCombobox::slotConfigChanged);
+
+    setCurrentIndex(0);
+    slotIndexChanged(0);
+}
+
+void TitlebarControlCombobox::slotIndexChanged(int index)
+{
+    if (index == 0) {
+        m_displayDocumentTitle->user_setProxyProperty(false);
+        m_displayDocumentNameOrPath->user_setProxyProperty(Okular::Settings::EnumDisplayDocumentNameOrPath::Name);
+    } else if (index == 1) {
+        m_displayDocumentTitle->user_setProxyProperty(false);
+        m_displayDocumentNameOrPath->user_setProxyProperty(Okular::Settings::EnumDisplayDocumentNameOrPath::Path);
+    } else if (index == 2) {
+        m_displayDocumentTitle->user_setProxyProperty(true);
+        m_displayDocumentNameOrPath->user_setProxyProperty(Okular::Settings::EnumDisplayDocumentNameOrPath::Name);
+    } else {
+        m_displayDocumentTitle->user_setProxyProperty(true);
+        m_displayDocumentNameOrPath->user_setProxyProperty(Okular::Settings::EnumDisplayDocumentNameOrPath::Path);
+    }
+}
+
+void TitlebarControlCombobox::slotConfigChanged()
+{
+    if (m_displayDocumentTitle->proxyPropertyValue() == false) {
+        if (m_displayDocumentNameOrPath->proxyPropertyValue() == Okular::Settings::EnumDisplayDocumentNameOrPath::Name) {
+            setCurrentIndex(0);
+        } else {
+            setCurrentIndex(1);
+        }
+    } else {
+        if (m_displayDocumentNameOrPath->proxyPropertyValue() == Okular::Settings::EnumDisplayDocumentNameOrPath::Name) {
+            setCurrentIndex(2);
+        } else {
+            setCurrentIndex(3);
+        }
+    }
 }
--- conf/dlggeneral.h
+++ conf/dlggeneral.h
@@ -10,7 +10,8 @@
 #ifndef _DLGGENERAL_H
 #define _DLGGENERAL_H
 
-#include <qwidget.h>
+#include <QComboBox>
+#include <QWidget>
 
 #include "part.h"
 
@@ -28,6 +29,95 @@ protected:
     void showEvent(QShowEvent *) override;
 
     Ui_DlgGeneralBase *m_dlg;
+};
+
+/**
+ * Proxy for a single kcfg_... property.
+ *
+ * KConfigDialogManager interfaces kcfg_... methods,
+ * user interfaces user_... methods.
+ */
+class KcfgPropertyProxy : public QWidget
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QVariant proxyProperty READ proxyPropertyValue WRITE kcfg_setProxyProperty NOTIFY kcfg_proxyPropertyChanged USER true)
+
+protected:
+    QVariant m_proxyPropertyValue;
+
+public:
+    /**
+     * @param propertyName How your property is called, something like @c "kcfg_ThisIsMyConfigKey".
+     */
+    explicit KcfgPropertyProxy(const QString &objectName, QWidget *parent)
+        : QWidget(parent)
+    {
+        setObjectName(objectName);
+    }
+
+    /**
+     * The current value of your property.
+     */
+    QVariant proxyPropertyValue()
+    {
+        return m_proxyPropertyValue;
+    }
+
+Q_SIGNALS:
+    /**
+     * This is emitted when the property changes in the configuration.
+     */
+    void user_proxyPropertyChanged();
+
+    /**
+     * KConfigDialogManager listens to this; don’t call yourself.
+     */
+    void kcfg_proxyPropertyChanged();
+
+public Q_SLOTS:
+    /**
+     * Call this to pass a new value to the configuration.
+     */
+    void user_setProxyProperty(const QVariant &v)
+    {
+        if (m_proxyPropertyValue != v) {
+            m_proxyPropertyValue = v;
+            emit kcfg_proxyPropertyChanged();
+        }
+    }
+
+    /**
+     * KConfigDialogManager uses this to give you new values; don’t call yourself.
+     */
+    void kcfg_setProxyProperty(const QVariant &v)
+    {
+        if (m_proxyPropertyValue != v) {
+            m_proxyPropertyValue = v;
+            emit user_proxyPropertyChanged();
+        }
+    }
+};
+
+/**
+ * Bundles kcfg_DisplayDocumentTitle and kcfg_DisplayDocumentNameOrPath in one combobox.
+ */
+class TitlebarControlCombobox : public QComboBox
+{
+    Q_OBJECT
+
+public:
+    explicit TitlebarControlCombobox(QWidget *parent);
+
+protected:
+    /** bool DisplayDocumentTitle */
+    KcfgPropertyProxy *m_displayDocumentTitle;
+    /** enum int DisplayDocumentNameOrPath */
+    KcfgPropertyProxy *m_displayDocumentNameOrPath;
+
+protected Q_SLOTS:
+    void slotIndexChanged(int index);
+    void slotConfigChanged();
 };
 
 #endif
