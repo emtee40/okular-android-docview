736d9c606b543091ccd6b099a187dc70cf4c8361
Albert Astals Cid <aacid@kde.org>
2020-09-05 19:20:59 +0200

    kwallet and kjs are mandatory in cmake anyway, remove ifdefs

--- part.cpp
+++ part.cpp
@@ -67,13 +67,11 @@
 #include <KStandardShortcut>
 #include <KToggleAction>
 #include <KToggleFullScreenAction>
-#include <Kdelibs4ConfigMigrator>
-#include <Kdelibs4Migration>
-#ifdef WITH_KWALLET
 #include <KWallet>
-#endif
 #include <KXMLGUIClient>
 #include <KXMLGUIFactory>
+#include <Kdelibs4ConfigMigrator>
+#include <Kdelibs4Migration>
 
 #if PURPOSE_FOUND
 #include <Purpose/AlternativesModel>
@@ -1373,7 +1371,6 @@ Document::OpenResult Part::doOpenFile(const QMimeType &mimeA, const QString &fil
         }
         m_documentOpenWithPassword = false;
 
-#ifdef WITH_KWALLET
         // if the file didn't open correctly it might be encrypted, so ask for a pass
         QString walletName, walletFolder, walletKey;
         m_document->walletDataForFile(fileNameToOpen, &walletName, &walletFolder, &walletKey);
@@ -1439,7 +1436,6 @@ Document::OpenResult Part::doOpenFile(const QMimeType &mimeA, const QString &fil
                 }
             }
         }
-#endif
     }
 
     if (openResult == Document::OpenSuccess) {
