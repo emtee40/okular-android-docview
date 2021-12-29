e3d1176677821990c94d68e263d1c49115c011e4
Albert Astals Cid <aacid@kde.org>
2021-12-29 09:55:56 +0100

    PDF: Fix memory leak when the file has Optional Content links

--- generators/poppler/generator_pdf.cpp
+++ generators/poppler/generator_pdf.cpp
@@ -1004,6 +1004,11 @@ void PDFGenerator::opaqueAction(const Okular::BackendOpaqueAction *action)
     pdfdoc->optionalContentModel()->applyLink(const_cast<Poppler::LinkOCGState *>(popplerLink));
 }
 
+void PDFGenerator::freeOpaqueActionContents(const Okular::BackendOpaqueAction &action)
+{
+    delete action.nativeId().value<const Poppler::LinkOCGState *>();
+}
+
 bool PDFGenerator::isAllowed(Okular::Permission permission) const
 {
     bool b = true;
