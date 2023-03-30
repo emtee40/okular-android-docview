651b8d4a3ed7b6f94535756bec2b38949c139880
Sune Vuorela <sune@vuorela.dk>
2023-03-30 22:00:28 +0200

    Split implementation

--- part/signaturepartutils.h
+++ part/signaturepartutils.h
@@ -7,11 +7,14 @@
 #ifndef OKULAR_SIGNATUREPARTUTILS_H
 #define OKULAR_SIGNATUREPARTUTILS_H
 
+#include <QDialog>
+#include <QStyledItemDelegate>
 #include <memory>
 
 #include "gui/signatureguiutils.h"
 
 class PageView;
+class QComboBox;
 
 namespace SignaturePartUtils
 {
@@ -19,6 +22,23 @@ std::unique_ptr<Okular::CertificateInfo> getCertificateAndPasswordForSigning(Pag
 QString getFileNameForNewSignedFile(PageView *pageView, Okular::Document *doc);
 void signUnsignedSignature(const Okular::FormFieldSignature *form, PageView *pageView, Okular::Document *doc);
 
+class KeyDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    using QStyledItemDelegate::QStyledItemDelegate;
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const final;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const final;
+};
+
+class SelectCertificateDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    QComboBox *combo;
+
+    SelectCertificateDialog(QWidget *parent);
+};
 }
 
 #endif
