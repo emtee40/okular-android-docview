9781f91166b47dfa992370195e9ce487e8472fd3
Sune Vuorela <sune@vuorela.dk>
2023-03-31 15:13:09 +0200

    PDF signature backend configuration and adaptions
     - Configuration UI for backend selection
     - If backend don't handle password itself, no reason to
       do the things with password

--- generators/poppler/generator_pdf.cpp
+++ generators/poppler/generator_pdf.cpp
@@ -640,6 +640,9 @@ PDFGenerator::PDFGenerator(QObject *parent, const QVariantList &args)
     if (!PDFSettings::useDefaultCertDB()) {
         Poppler::setNSSDir(QUrl(PDFSettings::dBCertificatePath()).toLocalFile());
     }
+#if POPPLER_VERSION_MACRO >= QT_VERSION_CHECK(23, 05, 0)
+    Poppler::setActiveBackend(PDFSettingsWidget::settingStringToPopplerEnum(PDFSettings::signatureBackend()));
+#endif
 }
 
 PDFGenerator::~PDFGenerator()
@@ -1997,7 +2000,11 @@ Okular::AnnotationProxy *PDFGenerator::annotationProxy() const
 
 bool PDFGenerator::canSign() const
 {
+#if POPPLER_VERSION_MACRO >= QT_VERSION_CHECK(23, 05, 0)
+    return !Poppler::availableBackends().empty();
+#else
     return Poppler::hasNSSSupport();
+#endif
 }
 
 bool PDFGenerator::sign(const Okular::NewSignatureData &oData, const QString &rFilename)
