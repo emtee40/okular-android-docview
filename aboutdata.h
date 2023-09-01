1c676ea99f06e6e99c33ec021e7a531806ce1df5
Natalie Clarius <natalie_clarius@yahoo.de>
2023-09-01 02:03:03 +0200

    aboutdata: add eco information

--- aboutdata.h
+++ aboutdata.h
@@ -38,7 +38,7 @@ inline KAboutData okularAboutData()
     about.addCredit(i18n("Jiri Baum - NICTA"), i18n("Table selection tool"), QStringLiteral("jiri@baum.com.au"));
     about.addCredit(i18n("Fabio D'Urso"), i18n("Annotation improvements"), QStringLiteral("fabiodurso@hotmail.it"));
 
-    about.setEcoCertifications(QList<KAboutEco::EcoCertification>() << KAboutEco::KDEEco << KAboutEco::BlueAngel);
+    about.setEcoCertifications(QList<KAboutData::EcoCertification>() << KAboutData::KDEEco << KAboutData::BlueAngel);
     about.setEnergyEfficiencyDataLink(QStringLiteral("https://invent.kde.org/teams/eco/blue-angel-application/-/blob/master/applications/okular/de-uz-215-eng-annex-7-okular.xml"));
     about.setOpenLicenseLink(QStringLiteral("https://invent.kde.org/graphics/okular/-/blob/master/COPYING"));
     about.setSourceCodeLink(QStringLiteral("https://invent.kde.org/graphics/okular"));
