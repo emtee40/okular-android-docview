dd01ce261ed83536fc9bd8f5c5a68b30a59fa9db
Carl Schwan <carl@carlschwan.eu>
2023-12-05 16:53:53 +0100

    Port away to newer KWindowSystem API

--- shell/okular_main.cpp
+++ shell/okular_main.cpp
@@ -23,6 +23,7 @@
 #include "config-okular.h"
 #if HAVE_X11
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <KX11Extras>
 #include <private/qtx11extras_p.h>
 #else
 #include <QX11Info>
@@ -107,7 +108,15 @@ static bool attachExistingInstance(const QStringList &paths, const QString &seri
     const QString pattern = QStringLiteral("org.kde.okular-");
     const QString myPid = QString::number(qApp->applicationPid());
     QScopedPointer<QDBusInterface> bestService;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#if HAVE_X11
+    const int desktop = KX11Extras::currentDesktop();
+#else
+    const int desktop = 0;
+#endif
+#else
     const int desktop = KWindowSystem::currentDesktop();
+#endif
 
     // Select the first instance that isn't us (metric may change in future)
     for (const QString &service : services) {
