7c599d7b2d2e8a2ff5675bac3d3e344ae4900abf
Sune Vuorela <sune@vuorela.dk>
2023-12-13 14:22:18 +0100

    Fix warning

--- generators/poppler/pdfsettingswidget.cpp
+++ generators/poppler/pdfsettingswidget.cpp
@@ -87,7 +87,7 @@ PDFSettingsWidget::PDFSettingsWidget(QWidget *parent)
         m_pdfsw.kcfg_SignatureBackend->setProperty("kcfg_property", QByteArray("currentData"));
 
         m_pdfsw.kcfg_SignatureBackend->setCurrentIndex(selected);
-        connect(m_pdfsw.kcfg_SignatureBackend, &QComboBox::currentTextChanged, [this](const QString &text) {
+        connect(m_pdfsw.kcfg_SignatureBackend, &QComboBox::currentTextChanged, this, [this](const QString &text) {
             auto backendEnum = settingStringToPopplerEnum(text);
             if (!backendEnum) {
                 return;
