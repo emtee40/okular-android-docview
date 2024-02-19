19879bce3adf8d15e3f4b7441f4164527b66880d
Ingo Klöcker <kloecker@kde.org>
2024-02-19 18:37:55 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- generators/poppler/pdfsettingswidget.cpp
+++ generators/poppler/pdfsettingswidget.cpp
@@ -39,7 +39,7 @@ static QString popplerEnumToUserString(Poppler::CryptoSignBackend backend)
     case Poppler::CryptoSignBackend::NSS:
         return QStringLiteral("NSS");
     case Poppler::CryptoSignBackend::GPG:
-        return QStringLiteral("GnuPG (S/Mime)");
+        return QStringLiteral("GnuPG (S/MIME)");
     }
     return {};
 }
