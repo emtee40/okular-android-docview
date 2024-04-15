60a05928542d72d87ddfa8707840adb056cbfaaf
Sune Vuorela <sune@vuorela.dk>
2024-04-15 16:38:27 +0200

    clang-format

--- generators/poppler/pdfsignatureutils.cpp
+++ generators/poppler/pdfsignatureutils.cpp
@@ -137,7 +137,7 @@ Okular::SignatureInfo::CertificateStatus fromPoppler(Poppler::SignatureValidatio
         return Okular::SignatureInfo::CertificateGenericError;
     case Poppler::SignatureValidationInfo::CertificateNotVerified:
         return Okular::SignatureInfo::CertificateNotVerified;
-#if POPPLER_VERSION_MACRO > QT_VERSION_CHECK(24,04,0)
+#if POPPLER_VERSION_MACRO > QT_VERSION_CHECK(24, 04, 0)
     case Poppler::SignatureValidationInfo::CertificateVerificationInProgress:
         return Okular::SignatureInfo::CertificateVerificationInProgress;
 #endif
